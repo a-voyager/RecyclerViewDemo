97d9a9dd01f6ea23ba6d7e1c4d100dfa8d6a8a64
wuhaojie <w19961009@126.com>
2015-08-21 18:46:07 +0800

    Base List.

--- app/src/main/java/com/voyager/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/voyager/recyclerviewdemo/MainActivity.java
@@ -1,37 +1,66 @@
 package com.voyager.recyclerviewdemo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
+
+    private RecyclerView rv_main;
+    private ArrayList<String> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        rv_main = (RecyclerView) findViewById(R.id.rv_main);
+        rv_main.setLayoutManager(new LinearLayoutManager(this));
+        rv_main.setAdapter(new MyAdapter());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+    private void init() {
+        data = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            data.add("这是第" + i + "个项目");
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder myViewHolder = new MyViewHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.rv_item, null, false));
+            return myViewHolder;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.textView.setText(data.get(position));
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            private TextView textView;
+
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                textView = (TextView) itemView.findViewById(R.id.tv_rv_item);
+            }
+        }
+
     }
+
 }
